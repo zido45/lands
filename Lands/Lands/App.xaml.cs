5bb2028c336048ba07dfbf6c94084d08ad6cb565
agustin donoso morales <adm@stiga.es>
2018-02-12 22:48:41 +0100

    prueba login apariencia solo diseño

---
+++ Lands/Lands/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+
+namespace Lands
+{
+    using Lands.Views;
+    using Xamarin.Forms;
+
+    public partial class App : Application
+	{
+        #region Ctor
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage( new LoginPage());
+        }
+        #endregion
+
+        #region Metodos
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        } 
+        #endregion
+    }
+}
